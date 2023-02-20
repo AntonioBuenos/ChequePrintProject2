e50ebb14af6cc6849b290f876ef49743e2bbb58a
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-20 13:23:50 +0300

    Add checks to PathGetter

--- src/main/java/by/smirnov/chequeprintproject/util/PathGetter.java
+++ src/main/java/by/smirnov/chequeprintproject/util/PathGetter.java
@@ -7,13 +7,17 @@ import java.io.File;
 @UtilityClass
 public class PathGetter {
 
+    private static final String DEFAULT_FILE_NAME = "cheque.txt";
     private static final String DIR = "user.dir";
+    private static final String EXTENSION = ".txt";
     private static final String PATH = "src" +
             File.separator + "main" +
             File.separator + "resources" +
             File.separator;
 
     public static String getPath(String filename) {
+        if(filename == null || filename.isBlank()) filename = DEFAULT_FILE_NAME;
+        if(!filename.endsWith(EXTENSION)) filename = filename + EXTENSION;
         String root = System.getProperty(DIR);
         return root + File.separator + PATH + filename;
     }
