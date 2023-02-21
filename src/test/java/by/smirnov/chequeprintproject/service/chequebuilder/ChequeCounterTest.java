c1bb99c29a043e250fdf0324ee46b4aa351ff093
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-21 16:17:22 +0300

    Add ChequeCounter getProducts test

--- src/test/java/by/smirnov/chequeprintproject/service/chequebuilder/ChequeCounterTest.java
+++ src/test/java/by/smirnov/chequeprintproject/service/chequebuilder/ChequeCounterTest.java
@@ -1,5 +1,6 @@
 package by.smirnov.chequeprintproject.service.chequebuilder;
 
+import by.smirnov.chequeprintproject.builder.DiscountCards;
 import by.smirnov.chequeprintproject.domain.DiscountCard;
 import by.smirnov.chequeprintproject.domain.Product;
 import org.junit.jupiter.api.DisplayName;
@@ -19,11 +20,19 @@ class ChequeCounterTest {
     private ChequeCounter counter;
 
     @Test
-    void getProducts() {
+    @DisplayName("GetProducts should return equal map")
+    void checkGetProductsShouldReturnEqualMap() {
+        Map<Product, Integer> expected = Map.of(aProduct().build(), 1, aProduct().id(2L).build(), 1);
+        counter = getCheckCounter();
+
+        Map<Product, Integer> actual = counter.getProducts();
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void getCard() {
+
     }
 
     @ParameterizedTest
@@ -44,8 +53,8 @@ class ChequeCounterTest {
     @DisplayName("getVatAmount should return result")
     @CsvFileSource(resources = "/test_cheque_vat_amount.csv", numLinesToSkip = 1, delimiter = ',')
     void checkGetVatAmountShouldReturnResult(double price1, boolean promo1, int qnty1,
-                      double price2, boolean promo2, int qnty2,
-                      double cardRate, double result) {
+                                             double price2, boolean promo2, int qnty2,
+                                             double cardRate, double result) {
         counter = getCheckCounter(price1, promo1, qnty1, price2, promo2, qnty2, cardRate);
 
         double actual = counter.getVatAmount();
@@ -57,8 +66,8 @@ class ChequeCounterTest {
     @DisplayName("getPromotionDiscountSum should return result")
     @CsvFileSource(resources = "/test_cheque_promotion_discount.csv", numLinesToSkip = 1, delimiter = ',')
     void checkGetPromotionDiscountSumShouldReturnResult(double price1, boolean promo1, int qnty1,
-                                 double price2, boolean promo2, int qnty2,
-                                 double cardRate, double result) {
+                                                        double price2, boolean promo2, int qnty2,
+                                                        double cardRate, double result) {
         counter = getCheckCounter(price1, promo1, qnty1, price2, promo2, qnty2, cardRate);
 
         double actual = counter.getPromotionDiscountSum();
@@ -70,8 +79,8 @@ class ChequeCounterTest {
     @DisplayName("getTaxableAmount should return result")
     @CsvFileSource(resources = "/test_cheque_taxable_amount.csv", numLinesToSkip = 1, delimiter = ',')
     void getTaxableAmountShouldReturnResult(double price1, boolean promo1, int qnty1,
-                          double price2, boolean promo2, int qnty2,
-                          double cardRate, double result) {
+                                            double price2, boolean promo2, int qnty2,
+                                            double cardRate, double result) {
         counter = getCheckCounter(price1, promo1, qnty1, price2, promo2, qnty2, cardRate);
 
         double actual = counter.getTaxableAmount();
@@ -83,8 +92,8 @@ class ChequeCounterTest {
     @DisplayName("getGrossChequeAmount should return result")
     @CsvFileSource(resources = "/test_cheque_gross_amount.csv", numLinesToSkip = 1, delimiter = ',')
     void getGrossChequeAmountShouldReturnResult(double price1, boolean promo1, int qnty1,
-                              double price2, boolean promo2, int qnty2,
-                              double cardRate, double result) {
+                                                double price2, boolean promo2, int qnty2,
+                                                double cardRate, double result) {
         counter = getCheckCounter(price1, promo1, qnty1, price2, promo2, qnty2, cardRate);
 
         double actual = counter.getGrossChequeAmount();
@@ -107,11 +116,19 @@ class ChequeCounterTest {
 
     private ChequeCounter getCheckCounter(double price1, boolean promo1, int qnty1,
                                           double price2, boolean promo2, int qnty2,
-                                          double cardRate){
+                                          double cardRate) {
         Map<Product, Integer> products = new HashMap<>();
         products.put(aProduct().price(price1).isPromoted(promo1).build(), qnty1);
         products.put(aProduct().id(2L).price(price2).isPromoted(promo2).build(), qnty2);
         DiscountCard card = aCard().discountRate(cardRate).build();
+        return new ChequeCounter(products, card);
+    }
+
+    private ChequeCounter getCheckCounter() {
+        Map<Product, Integer> products = new HashMap<>();
+        products.put(aProduct().build(), 1);
+        products.put(aProduct().id(2L).build(), 1);
+        DiscountCard card = aCard().build();
         return new ChequeCounter(products, card);
     }
 }
