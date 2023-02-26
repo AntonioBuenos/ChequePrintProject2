d21b3dba46d74b69ac090699f8eb0bac4e631d13
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-26 11:19:16 +0300

    Add LFUCache, add NodeCache & refactor caches

--- src/main/java/by/smirnov/chequeprintproject/repository/cache/LRUCache.java
+++ src/main/java/by/smirnov/chequeprintproject/repository/cache/LRUCache.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,18 +14,19 @@ import java.util.Map;
 import static by.smirnov.chequeprintproject.exceptionhandler.ExceptionConstants.LOG_MESSAGE_CACHE_NOT_NORMALIZED;
 
 @Component
+@Primary
 @Slf4j
-public class LFUCache extends NodeCache implements DiscountCardCache {
+public class LRUCache extends NodeCache implements DiscountCardCache {
 
     @Override
     public DiscountCard put(DiscountCard card) {
         Long key = card.getId();
         if (map.containsKey(key)) {
-            Node node = new Node(card, map.get(key).getCount() + 1);
+            Node node = new Node(card, getTime());
             map.compute(key, (k, v) -> v = node);
         } else {
             normalizeCacheSize();
-            map.put(key, new Node(card, 1L));
+            map.put(key, new Node(card, getTime()));
         }
         return card;
     }
---
+++ src/main/java/by/smirnov/chequeprintproject/repository/cache/NodeCache.java
@@ -0,0 +1,52 @@
+package by.smirnov.chequeprintproject.repository.cache;
+
+import by.smirnov.chequeprintproject.domain.DiscountCard;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+import static by.smirnov.chequeprintproject.exceptionhandler.ExceptionConstants.LOG_MESSAGE_CACHE_NOT_NORMALIZED;
+
+@Slf4j
+public abstract class NodeCache {
+
+    Map<Long, Node> map = new HashMap<>();
+    private static final int MAX_ENTRIES = 3;
+
+    public DiscountCard findById(Long id) {
+        Node node = map.get(id);
+        if(node==null) return null;
+        put(node.getValue());
+        System.out.println(map.toString());
+        return node.getValue();
+    }
+
+    public abstract DiscountCard put(DiscountCard card);
+
+    public void delete(Long id) {
+        map.remove(id);
+    }
+
+    void normalizeCacheSize() {
+        if (map.size() >= MAX_ENTRIES) {
+            try{
+                Long rarelyUsedId = map.values().stream().
+                        min(Comparator.comparing(Node::getCount))
+                        .orElseThrow(NullPointerException::new)
+                        .getValue()
+                        .getId();
+                map.remove(rarelyUsedId);
+            }catch (NullPointerException e){
+                log.error(LOG_MESSAGE_CACHE_NOT_NORMALIZED, e.getMessage());
+            }
+        }
+    }
+
+    long getTime(){
+        return Timestamp.valueOf(LocalDateTime.now()).getTime();
+    }
+}
