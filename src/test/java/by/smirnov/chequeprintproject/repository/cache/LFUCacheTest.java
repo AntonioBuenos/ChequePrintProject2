2c90afb283d9d56327b81cf493f5ef2cf99b3c1c
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-02-27 09:52:48 +0300

    Change LFUCacheTest with test builder

--- src/test/java/by/smirnov/chequeprintproject/repository/cache/LFUCacheTest.java
+++ src/test/java/by/smirnov/chequeprintproject/repository/cache/LFUCacheTest.java
@@ -1,17 +1,15 @@
 package by.smirnov.chequeprintproject.repository.cache;
 
 import by.smirnov.chequeprintproject.domain.DiscountCard;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static by.smirnov.chequeprintproject.builder.DiscountCards.aCard;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LFUCacheTest {
 
@@ -19,27 +17,9 @@ class LFUCacheTest {
 
     @BeforeEach
     void init(){
-        cache.map.put(1L, new Node(DiscountCard.builder()
-                .id(1L)
-                .holderName("Mescalero El Camino")
-                .holderEmail("1005@supermail.com")
-                .discountRate(3.0)
-                .isActive(true)
-                .build(), 1L));
-        cache.map.put(2L, new Node(DiscountCard.builder()
-                .id(2L)
-                .holderName("Mescalero El Camino")
-                .holderEmail("1005@supermail.com")
-                .discountRate(4.0)
-                .isActive(true)
-                .build(), 2L));
-        cache.map.put(3L, new Node(DiscountCard.builder()
-                .id(3L)
-                .holderName("Mescalero El Camino")
-                .holderEmail("1005@supermail.com")
-                .discountRate(5.0)
-                .isActive(true)
-                .build(), 3L));
+        cache.map.put(1L, new Node(aCard().id(1L).build(), 1L));
+        cache.map.put(2L, new Node(aCard().id(2L).build(), 2L));
+        cache.map.put(3L, new Node(aCard().id(3L).build(), 3L));
     }
 
     @ParameterizedTest
