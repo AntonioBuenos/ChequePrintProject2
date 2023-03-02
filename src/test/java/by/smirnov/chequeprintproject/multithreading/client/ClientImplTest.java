58dd7c0ca230218c79dd8e08b0a5855f24c7e2bc
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-02 13:52:41 +0300

    Amend ClientImplTest

--- src/test/java/by/smirnov/chequeprintproject/multithreading/client/ClientImplTest.java
+++ src/test/java/by/smirnov/chequeprintproject/multithreading/client/ClientImplTest.java
@@ -3,6 +3,8 @@ package by.smirnov.chequeprintproject.multithreading.client;
 import by.smirnov.chequeprintproject.multithreading.entity.Request;
 import by.smirnov.chequeprintproject.multithreading.entity.Response;
 import by.smirnov.chequeprintproject.multithreading.server.ServerImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,7 +15,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static by.smirnov.chequeprintproject.multithreading.constants.Constants.TEST_REQUEST_AMOUNT;
-import static by.smirnov.chequeprintproject.multithreading.constants.MultithreadingConstants.INCREMENT_QTY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
@@ -29,17 +30,35 @@ class ClientImplTest {
     @InjectMocks
     private ClientImpl client;
 
-    @Test
-    void sendRequests() {
-        int entriesQty = 10000;
-        List<Request> requests = Stream.generate(() -> new Request(1, TEST_REQUEST_AMOUNT))
+    private static List<Request> requests;
+
+    private static final int entriesQty = 10000;
+
+    @BeforeAll
+    static void init(){
+        requests = Stream.generate(() -> new Request(1, TEST_REQUEST_AMOUNT))
                 .limit(entriesQty)
                 .toList();
-        doReturn(new Response(1, TEST_REQUEST_AMOUNT + INCREMENT_QTY));
+    }
+
+    @Test
+    @DisplayName("Returned list size should equal entries qty")
+    void checkReturnedListSizeShouldEqualEntriesQty() {
+        doReturn(new Response(1, 1)).when(server).proceed(any(Request.class));
 
         List<Response> responses = client.sendRequests(requests);
 
         verify(server, times(entriesQty)).proceed(any(Request.class));
         assertThat(responses).hasSize(entriesQty);
+    }
+
+    @Test
+    @DisplayName("sendRequests should send to server all the entries")
+    void checkSendRequestsShouldSendToServerAllTheEntries() {
+        doReturn(new Response(1, 1)).when(server).proceed(any(Request.class));
+
+        client.sendRequests(requests);
+
+        verify(server, times(entriesQty)).proceed(any(Request.class));
     }
 }
