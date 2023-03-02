66f8930452795c85b9d9ccf53d99720b704a031c
Anton Smirnov <watashinodenwadesu@gmail.com>
2023-03-02 13:33:12 +0300

    Realise ServerImpl

--- src/main/java/by/smirnov/chequeprintproject/multithreading/server/ServerImpl.java
+++ src/main/java/by/smirnov/chequeprintproject/multithreading/server/ServerImpl.java
@@ -2,10 +2,38 @@ package by.smirnov.chequeprintproject.multithreading.server;
 
 import by.smirnov.chequeprintproject.multithreading.entity.Request;
 import by.smirnov.chequeprintproject.multithreading.entity.Response;
+import by.smirnov.chequeprintproject.multithreading.utils.Sleeper;
+import org.springframework.stereotype.Component;
 
-public class ServerImpl implements Server{
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static by.smirnov.chequeprintproject.multithreading.constants.MultithreadingConstants.INCREMENT_QTY;
+
+@Component
+public class ServerImpl implements Server {
+
+    private static final Lock lock = new ReentrantLock();
+    private static int bufferAmount = 0;
+
     @Override
     public Response proceed(Request request) {
-        return null;
+        int newAmount = doWork(request.getAmount());
+        return new Response(request.getId(), newAmount);
+    }
+
+    private static int doWork(int amount) {
+        lock.lock();
+        try{
+            for (int i = 0; i < INCREMENT_QTY; i++) {
+                Sleeper.sleep();
+                ++bufferAmount;
+            }
+            amount += bufferAmount;
+            bufferAmount = 0;
+        } finally {
+            lock.unlock();
+        }
+        return amount;
     }
 }
